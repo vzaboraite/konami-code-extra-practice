414ee545df8d108eb54d0c17fae6f6cdc0bbdbdd
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-26 23:13:44 +0100

    Complete main exercise.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,56 @@ Tips
 // input: array of konamiCode
 // output: -
 
+const konamiCode = [
+  "up",
+  "up",
+  "down",
+  "down",
+  "left",
+  "right",
+  "left",
+  "right",
+  "B",
+  "A",
+];
 
+// konamiSequence of keyboard input event keys
+//  => https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+const konamiSequence = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+];
+
+function renderSequenceSection(array) {
+  const sectionElem = document.createElement("section");
+  console.log(sectionElem);
+  document.body.append(sectionElem);
+
+  const sequenceContainerElem = document.createElement("ul");
+  sequenceContainerElem.className = "code-sequence";
+  sectionElem.append(sequenceContainerElem);
+
+  for (let i = 0; i < array.length; i++) {
+    const key = array[i];
+    const keyListItemElem = document.createElement("li");
+    keyListItemElem.setAttribute("id", "key");
+    keyListItemElem.className = "code-key";
+    keyListItemElem.setAttribute("style", "list-style-type: none");
+
+    keyListItemElem.innerText = key;
+
+    sequenceContainerElem.append(keyListItemElem);
+  }
+}
+renderSequenceSection(konamiCode);
 
 // 2.0 Add event listener for the keyboard inputs
 // => https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
@@ -27,11 +76,32 @@ Tips
 // - keep track of user inputs:
 //  - declare variable and assign user input (userSequence[])
 
+let userSequence = [];
+document.addEventListener("keydown", (event) => {
+  console.log("key was pressed: ", event.key);
+  userSequence.push(event.key);
+  checkSequences(konamiSequence, userSequence);
+  console.log(userSequence);
+});
 
 // 3.0 check user input
 
 // input: konamiSequence and userSequence arrays
 // output: -
-//  if the sequence is wrong, reset the code
-//  if the sequence is right, make logo spin
-//  - use bridge to <img> - ".konami-logo"
+function checkSequences(konamiSeq, userSeq) {
+  for (let i = 0; i < userSeq.length; i++) {
+    //  if the sequence is wrong, reset the code
+    if (userSeq[i] !== konamiSeq[i]) {
+      userSequence = [];
+      break;
+    }
+  }
+  //  if the sequence is right, make logo spin
+  //  - use bridge to <img> - ".konami-logo"
+  if (userSequence.length === konamiSequence.length) {
+    const konamiLogoElem = document.querySelector(".konami-logo");
+    // add second class to the element using .classList.add()
+    // =>https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+    konamiLogoElem.classList.add("combo");
+  }
+}
