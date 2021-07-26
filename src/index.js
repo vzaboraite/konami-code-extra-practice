950f653d043516c1f955271a25a69251a35828dd
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-26 23:06:35 +0100

    Add instructions.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // https://en.wikipedia.org/wiki/Konami_Code
+
+/*
+- Create a section after the logo with the code sequence inside as a reference for the User, You can come up with the style for it
+- Listen for keyboard inputs, and keep track if the user is pressing the keys sequence in the right order
+- If the User input the right sequence, make the logo spin
+
+Tips
+- Think about how you can keep track of the user inputs. The code should reset if you make a mistake
+- HTML and CSS can be changed with Javascript
+*/
