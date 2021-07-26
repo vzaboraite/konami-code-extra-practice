8f9d896329afe393202858f484cc80d2ae70ffd3
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-26 23:12:24 +0100

    Add pseudo-code.

--- src/index.js
+++ src/index.js
@@ -9,3 +9,29 @@ Tips
 - Think about how you can keep track of the user inputs. The code should reset if you make a mistake
 - HTML and CSS can be changed with Javascript
 */
+
+// 1.0 renderSequenceSection(keys)
+// create <section> and append to document.body (has to appear after <img> in HTML)
+// code sequence as reference for the user (up, up, down, down, left, right, left, right, B, A)
+// - create an array of konamiCode, render it on the page
+// - using for loop create <li> elements with innerText that is a konamiCode key?, append element to <section> element
+
+// input: array of konamiCode
+// output: -
+
+
+
+// 2.0 Add event listener for the keyboard inputs
+// => https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+// => do research on keydown vs keyup events
+// - keep track of user inputs:
+//  - declare variable and assign user input (userSequence[])
+
+
+// 3.0 check user input
+
+// input: konamiSequence and userSequence arrays
+// output: -
+//  if the sequence is wrong, reset the code
+//  if the sequence is right, make logo spin
+//  - use bridge to <img> - ".konami-logo"
